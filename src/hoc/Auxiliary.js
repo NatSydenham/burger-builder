63ff1ae32dc06c6a9cdb7b1217ebe2cca519dfe9
Nat <nathanial.sydenham@gmail.com>
2020-02-18 19:42:46 +0000

    added layout and aux components

---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+// Aux is a higher order component which simply returns its children in order to allow
+// us to have a wrapping root element
+
+const aux = (props) => props.children;
+export default aux;
